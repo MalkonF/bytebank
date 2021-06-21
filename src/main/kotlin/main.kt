5d8d1e4615d42f1d18166fc8033489b5cfb27f98
MalkonF <sudo777@protonmail.com>
2021-06-21 20:17:57 -0300

    Restrição das variáveis

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,11 @@
 fun main(){
     println("Bem vindo")
-    val titular = "Alex"
+    val titular: String = "Alex"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0 // resultado é em double
     println("titular:"+ titular)
     println("Titular $titular")//string template
+    println("Número da conta: $numeroConta")
+    println("Saldo da conta: $saldo")
 }
