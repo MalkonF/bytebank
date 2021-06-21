fc24b31937d30df98536f6288227d2e5838b9859
MalkonF <sudo777@protonmail.com>
2021-06-21 20:26:09 -0300

    Utilizando estruturas condicionais

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,8 +4,17 @@ fun main(){
     val numeroConta = 1000
     var saldo = 0.0
     saldo = 100 + 2.0 // resultado é em double
+    saldo = 0.0
+    saldo -=300.00
     println("titular:"+ titular)
     println("Titular $titular")//string template
     println("Número da conta: $numeroConta")
     println("Saldo da conta: $saldo")
+
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("Conta é neutra")
+        else -> println("Conta é negativa")
+    }
+    
 }
