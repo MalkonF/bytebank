9094fd4dbfab496a65c33eb756f6a37e6b0d9769
MalkonF <sudo777@protonmail.com>
2021-06-21 20:44:39 -0300

    Utilizando estruturas de repetição

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,45 @@
-fun main(){
+fun main() {
     println("Bem vindo")
-    val titular: String = "Alex"
-    val numeroConta = 1000
-    var saldo = 0.0
-    saldo = 100 + 2.0 // resultado é em double
-    saldo = 0.0
-    saldo -=300.00
-    println("titular:"+ titular)
-    println("Titular $titular")//string template
-    println("Número da conta: $numeroConta")
-    println("Saldo da conta: $saldo")
-
-    when {
-        saldo > 0.0 -> println("Conta é positiva")
-        saldo == 0.0 -> println("Conta é neutra")
-        else -> println("Conta é negativa")
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Alex $i"
+        val numeroConta = 1000 + i
+        var saldo = i + 10.0
+        saldo = 100 + 2.0 // resultado é em double
+        saldo = 0.0
+        saldo -= 300.00
+        println("titular:" + titular)
+        println("Titular $titular")//string template
+        println("Número da conta: $numeroConta")
+        println("Saldo da conta: $saldo")
+        println()
+        i++
     }
-    
+    /*for (i in 1..5) { //or for(i in 5 downTo 1 step 2){
+        if (i == 4) {
+            break
+        }
+        val titular: String = "Alex $i"
+        val numeroConta = 1000 + i
+        var saldo = i + 10.0
+        saldo = 100 + 2.0 // resultado é em double
+        saldo = 0.0
+        saldo -= 300.00
+        println("titular:" + titular)
+        println("Titular $titular")//string template
+        println("Número da conta: $numeroConta")
+        println("Saldo da conta: $saldo")
+        println()
+    }*/
+
+    // testaCondicoes(saldo)
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("Conta é positiva")
+    } else if (saldo == 0.0) {
+        println("Conta é neutra")
+    } else
+        println("Conta é negativa")
 }
