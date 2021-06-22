6480f5fdb5cda09ecb6ba92e3909bb6ed7ba539a
MalkonF <sudo777@protonmail.com>
2021-06-22 17:51:44 -0300

    Função de depósito

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,24 @@
 fun main() {
+testaCopiasEReferencias()
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaCopiasEReferencias() {
     val numeroX = 0
     var numeroY = numeroX
     numeroY++
     println("NumeroX $numeroX")
-    print("NumeroY $numeroY")
+    println("NumeroY $numeroY")
 
     val contaJoao = Conta()
     contaJoao.titular = "João"
-    var contaMaria = contaJoao //contaMaria fica com endereço de contaJoao. Qualquer alteração nela reflete joao
+    //var contaMaria = contaJoao //contaMaria fica com endereço de contaJoao. Qualquer alteração nela reflete joao
+    var contaMaria = Conta()
     contaMaria.titular = "Maria"
     var contaMaria2 = Conta() //instancia um novo obj Conta com novas posições na memória
 
@@ -16,10 +27,16 @@ fun main() {
 
     println(contaMaria) //vao ter os mesmo hashs, mesma referência na memoria
     println(contaJoao)
+
+    println("Depositando na conta do João")
+    deposita(contaJoao, 50.0)
+    println(contaJoao.saldo)
+    println("Depositando na conta da Maria")
+    deposita(contaMaria, 70.0)
+    println(contaMaria.saldo)
 }
 
-class Conta {
-    var titular = ""
-    var numero = 0
-    var saldo = 0.0
+fun deposita(conta: Conta, valor: Double) {
+    conta.saldo += valor
+
 }
